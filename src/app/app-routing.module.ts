c92248a0cc28411b9c2a216cfda8c1b0214309c4
kisho1131 <sumitkishore0131@gmail.com>
2022-10-08 00:33:29 +0530

    #comment updated app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, pathMatch:'full'},
-  { path: 'login', component : LoginComponent, pathMatch:'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'register', component: RegisterComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
